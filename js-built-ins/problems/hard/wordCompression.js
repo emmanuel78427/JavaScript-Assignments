6e3b9557c4141564f85fbf1602e4a23a76c0b668
emmanuel78427 <emmanuel78427@gmail.com>
2026-02-18 02:06:33 +0530

    solved wordCompression.js

--- js-built-ins/problems/hard/wordCompression.js
+++ js-built-ins/problems/hard/wordCompression.js
@@ -23,8 +23,30 @@
 
 
 function compressWords(arr) {
-  // Your code here
+  if (arr.length === 0) return [];
+   let result = [];
+   let count = 1;
+   for ( let i = 0; i<arr.length; i++) {
+    if (arr[i] === arr[i+1]) {
+      count++
+    } else {
+      if ( count > 1) {
+        result.push(arr[i] + count)
+      } else {
+        result.push(arr[i])
+      }
+      count = 1
+    }
+   }
+   return result;
 }
 
 
 module.exports = compressWords;
+
+console.log(compressWords(["apple", "apple", "banana", "banana", "banana", "cherry", "cherry", "apple", "apple"]))
+
+
+
+
+
