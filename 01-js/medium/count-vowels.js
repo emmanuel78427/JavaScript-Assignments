4615a9ec88a9ca5087c6069b7585e91a78b91414
emmanuel78427 <emmanuel78427@gmail.com>
2026-02-19 23:23:44 +0530

    solved count-vowels.js

--- 01-js/medium/count-vowels.js
+++ 01-js/medium/count-vowels.js
@@ -6,7 +6,15 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let vowels = "aeiouAEIOU"
+  let count = 0;
+  for( let i = 0; i< str.length; i++){
+    if(vowels.includes(str[i])){
+      count++
+    }
+  }return count
 }
+
+console.log(countVowels("Emmanuel"))
 
 module.exports = countVowels;
