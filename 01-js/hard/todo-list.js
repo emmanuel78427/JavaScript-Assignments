abdbc0fa31152a8c4531ac0f391561a64e5c9da7
emmanuel78427 <emmanuel78427@gmail.com>
2026-02-21 15:07:27 +0530

    solved todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,44 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todos = []
+  }
+add(todo) {
+  this.todos.push(todo);
+}
+remove(indexOfTodo) { 
+  if(indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1)              // it deletes 2 consecutive elements indexOfTodo and next element
+  }       
+}
+update(index, updatedTodo)  {
+  if(index >= 0 && index < this.todos.length ) {
+     this.todos[index] = updatedTodo
+  } return null
+}
+get(indexOfTodo) {
+  if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo]
+  } return null                                                        // need to write this as it is giving data , it come when if condition fails
+}
+getAll() {
+  return this.todos;
+}
+clear() {
+  this.todos = []
+}
 }
 
 module.exports = Todo;
+
+let TodoList = new Todo() 
+TodoList.add("Have to go to shopping mall") 
+TodoList.add("Have to meet my frnd") 
+TodoList.add("have to attend the meeting")
+
+TodoList.remove(0)
+
+
+console.log(TodoList.getAll())
+
